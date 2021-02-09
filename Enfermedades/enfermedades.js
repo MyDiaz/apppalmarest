0a4eb6bea3e2d1b2d64688b2f2c658078ea471ca
MyDiaz <diazmaya947@ymail.com>
2021-02-09 18:34:17 -0500

    Separación
    separar funcionalidades relacionadas a enfermedades sin etapas de enfermedades con etapas.

--- Enfermedades/enfermedades.js
+++ Enfermedades/enfermedades.js
@@ -5,20 +5,16 @@ const rutas = express.Router();
 
 const BaseDatos = new Pool(config.connectionData);
 
-//retorna las enfermedades y sus etapas registradas en la base de datos
-var get_enfermedades_etapas = async() => {
-    let consulta = 'SELECT * FROM "ETAPAS_ENFERMEDAD";';
-    const cliente_bd = await BaseDatos.connect();
-    let rta = await cliente_bd.query(consulta);
-    cliente_bd.release();
-    return rta;
-}
 
 var get_enfermedades = async() => {
-    let consulta = `SELECT * FROM "ENFERMEDAD" where procedimiento_tratamiento_enfermedad != 'NULL';`;
+    let consulta = `SELECT * FROM "ENFERMEDAD" where procedimiento_tratamiento_enfermedad != 'NULL' 
+    and 
+    fue_borrado = false;`;
+    console.log(consulta);
     const cliente_bd = await BaseDatos.connect();
     let rta = await cliente_bd.query(consulta);
     cliente_bd.release();
+    console.log("get_enfermedades", rta);
     return rta;
 }
 
@@ -31,17 +27,6 @@ var get_enfermedad = async(nombre_enfermedad) => {
     return rta.rows;
 }
 
-var get_enfermedad_etapa = async(nombre_enfermedad) => {
-    //id_enfermedad = id_enfermedad.toString()
-    let consulta = `SELECT * FROM "ETAPAS_ENFERMEDAD" where nombre_enfermedad = '${nombre_enfermedad}';`;
-    console.log(consulta);
-    const cliente_bd = await BaseDatos.connect();
-    let rta = await cliente_bd.query(consulta);
-    cliente_bd.release();
-    return rta.rows;
-}
-
-
 var post_enfermedad = async(req) => {
     let consulta = `INSERT INTO public."ENFERMEDAD"( nombre_enfermedad,
     "procedimiento_tratamiento_enfermedad") VALUES 
@@ -50,54 +35,6 @@ var post_enfermedad = async(req) => {
     let rta = await cliente_bd.query(consulta);
     cliente_bd.release();
     return rta;
-}
-
-var post_etapas_enfermedad = async(req) => {
-    let values = "";
-    let numero_etapas = req.body.etapas_enfermedad.length;
-    var enfermedad;
-    let nombre_enfermedad_decode = decodeURIComponent(req.body.nombre_enfermedad);
-    await get_enfermedad(nombre_enfermedad_decode).then(rta => {
-        enfermedad = rta;
-        console.log("enfermedad", enfermedad);
-    }).catch(err => {
-        console.log(err);
-    });
-    console.log("enfermedad afuera", enfermedad);
-    if (enfermedad.length == 0) {
-        let values = "";
-        for (let i = 0; i < numero_etapas; i = i + 1) {
-            values = values + `('${decodeURIComponent(req.body.etapas_enfermedad[i])}','${nombre_enfermedad_decode}', 
-                                    '${decodeURIComponent(req.body.tratamiento_etapa_enfermedad[i])}'),`
-        }
-        let consulta_enfermedad = `INSERT INTO public."ENFERMEDAD"(nombre_enfermedad) VALUES 
-            ('${nombre_enfermedad_decode}');`;
-        let consulta_etapas = `INSERT INTO public."ETAPAS_ENFERMEDAD" 
-            (etapa_enfermedad, nombre_enfermedad, tratamiento_etapa_enfermedad)
-            VALUES ${values}`;
-        consulta_etapas = consulta_etapas.slice(0, -1) + ";";
-        let consulta = `${consulta_enfermedad} ${consulta_etapas}`;
-        console.log("consulta", consulta);
-        const cliente_bd = await BaseDatos.connect();
-        var resp = await cliente_bd.query(consulta);
-        cliente_bd.release();
-        console.log("resp", resp);
-        return resp;
-    } else {
-        console.log("ya esta la enfermedad");
-        for (let i = 0; i < numero_etapas; i = i + 1) {
-            values = values + `('${decodeURIComponent(req.body.etapas_enfermedad[i])}','${nombre_enfermedad_decode}', 
-                                    '${decodeURIComponent(req.body.tratamiento_etapa_enfermedad[i])}'),`
-        }
-        let consulta = `INSERT INTO public."ETAPAS_ENFERMEDAD" 
-            (etapa_enfermedad, nombre_enfermedad, tratamiento_etapa_enfermedad)
-            VALUES ${values}`;
-        consulta = consulta.slice(0, -1) + ";";
-        const cliente_bd = await BaseDatos.connect();
-        var rta = await cliente_bd.query(consulta);
-        cliente_bd.release();
-        return rta;
-    }
 }
 
 //req.params.nombre_enfermedad
@@ -113,9 +50,10 @@ var put_enfermedad = async(req) => {
     return true;
 }
 
-
 var eliminar_enfermedad = async(nombre_enfermedad) => {
-    let consulta = `DELETE FROM public."ENFERMEDAD" WHERE nombre_enfermedad = '${nombre_enfermedad}';`;
+    let consulta = `UPDATE public."ENFERMEDAD"
+	SET fue_borrado = true
+	WHERE nombre_enfermedad='${nombre_enfermedad}';`;
     console.log(consulta);
     const cliente_bd = await BaseDatos.connect();
     let rta = await cliente_bd.query(consulta);
@@ -198,63 +136,6 @@ rutas.route('/enfermedad/:nombre_enfermedad')
             console.log("req.params", req.params);
             res.status(500).send({ message: 'Algo inesperado ocurrió.' })
         })
-    })
-
-rutas.route('/enfermedad-etapas')
-    .get((req, res) => {
-        get_enfermedades_etapas().then(rta => {
-            if (!rta) {
-                res.status(400).send({ message: 'No se pudo obtener el listado de enfermedades' });
-            } else {
-                res.status(200).send(rta.rows);
-            }
-        }).catch(
-            err => { res.status(400).send({ message: 'Algo inesperado ocurrió' }); }
-        )
-    })
-    .post((req, res) => {
-        function estaVacio(elemento) {
-            return elemento === '';
-        }
-        //si es TRUE es pq el array req.body.tratamiento_etapa_enfermedad tiene elementos
-        algo_tratamiento = req.body.tratamiento_etapa_enfermedad.findIndex(estaVacio) == -1;
-        algo_etapas = req.body.etapas_enfermedad.findIndex(estaVacio) == -1;
-        /*console.log("algo_tratamiento",algo_tratamiento);
-        console.log("algo_etapas",algo_etapas);
-        console.log(req.body);
-        console.log("etpas", req.body.etapas_enfermedad.length);
-        console.log("trta", req.body.tratamiento_etapa_enfermedad.length);*/
-        if (!req.body.nombre_enfermedad || !algo_tratamiento || !algo_etapas) {
-            res.status(400).send({ message: 'Ingrese todos los campos' });
-        } else {
-            post_etapas_enfermedad(req).then(rta => {
-                console.log("rta en post", rta);
-                if (!rta) {
-                    res.status(400).send({ message: 'No se pudo insertar la enfermedad' });
-                } else {
-                    res.status(200).send({ message: 'Se agregó correctamente' });
-                }
-            }).catch(err => {
-                res.status(500).send({ message: 'Algo inesperado ocurrió' })
-                console.log(err);
-            });
-        }
-    })
-
-rutas.route('/enfermedad-etapas/:nombre_enfermedad')
-    .get((req, res) => {
-        get_enfermedad_etapa(decodeURIComponent(req.params.nombre_enfermedad)).then(rta => {
-            if (!rta) {
-                res.status(400).send({ message: 'No se pudo obtener el listado de enfermedades' });
-            } else {
-                res.status(200).send(rta);
-            }
-        }).catch(
-            err => {
-                res.status(400).send({ message: 'Algo inesperado ocurrió' });
-                console.log(err);
-            }
-        )
     })
 
 module.exports = rutas;
