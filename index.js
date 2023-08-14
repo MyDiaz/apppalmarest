f781445f718d19bbd5ff2f6545315cf941061813
ajulianavil <33266101+Chiasaurio@users.noreply.github.com>
2023-08-14 13:10:16 -0500

    Agregar fertilizacion y fertilizaciones diarias route.

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ const movilTratamientos = require("./Movil/tratamientos")
 const movilPlateos = require("./Movil/plateos")
 const movilPodas = require("./Movil/podas")
 const movilViajes = require("./Movil/viajes")
+const movilFertilizantes = require("./Movil/fertilizantes")
+const movilFertilizaciones = require("./Movil/fertilizaciones")
 //permite que a todos las peticiones se pueda acceder a la propiedad body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -65,7 +67,8 @@ app.use(movilEnfermedades);
 app.use(movilPlagas);
 app.use(movilAgroquimicos);
 app.use(movilPalmas);
-app.use(movilCosechas);
+app.use(movilFertilizantes);
+app.use(movilFertilizaciones);
 app.use(viajes);
 app.use(censos)
 app.use(movilTratamientos);
