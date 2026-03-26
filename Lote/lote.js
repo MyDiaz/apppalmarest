479faec68d0fd4d174aace86e58dbdda879a1513
Mayra Riascos Diaz <diazmaya947@ymail.com>
2026-03-25 22:52:57 -0300

    funcionalidad agregada

--- Lote/lote.js
+++ Lote/lote.js
@@ -8,7 +8,7 @@ const BaseDatos = new Pool(config.connectionData);
 
 //retorna el nombre de todos los lotes registrados en la base de datos
 var get_lotes = async () => {
-  let consulta = 'SELECT * FROM "LOTE";';
+  let consulta = 'SELECT * FROM "LOTE" where fue_borrado = false;';
   const cliente_bd = await BaseDatos.connect();
   let rta = await cliente_bd.query(consulta);
   cliente_bd.release();
@@ -67,7 +67,19 @@ var update_mapa_lote = async (req) => {
   return rta;
 };
 
-//Actualiza los datos de un lote en especifico
+//borrado suave de un lote
+var soft_delete_lote = async (nombre_lote) => {
+  let consulta = `UPDATE public."LOTE"
+    SET fue_borrado = true
+    WHERE nombre_lote = $1;`;
+  const cliente_bd = await BaseDatos.connect();
+  let rta = await cliente_bd.query(consulta, [
+    decodeURIComponent(nombre_lote),
+  ]);
+  cliente_bd.release();
+  return rta;
+};
+
 var put_lote = async (req) => {
   let consulta = `UPDATE "LOTE" SET 
     año_siembra= $1, 
@@ -98,13 +110,13 @@ rutas
         if (!rta) {
           res
             .status(400)
-            .send({ message: "No se pudo obtener el listado de lotes" });
+            .send({ message: "No se pudo obtener el listado de lotes." });
         } else {
           res.status(200).send(rta.rows);
         }
       })
       .catch((err) => {
-        res.status(400).send({ message: `Algo inesperado ocurrió` });
+        res.status(400).send({ message: `Algo inesperado ocurrió.` });
         console.log(err);
       });
   })
@@ -117,34 +129,34 @@ rutas
       !req.body.numero_palmas ||
       !req.body.material_siembra
     ) {
-      res.status(400).send({ message: "Todos los campos son requeridos" });
+      res.status(400).send({ message: "Todos los campos son requeridos." });
     } else {
       post_lote(req)
         .then((rta) => {
-          res.status(200).send({ message: "El lote se insertó correctamente" });
+          res.status(200).send({ message: "El lote se insertó correctamente." });
         })
         .catch((err) => {
           console.log(err);
           var text;
           switch (err.constraint) {
             case "hectareas_check":
-              text = "Las hectareas deben ser mayor a cero";
+              text = "Las hectáreas deben ser mayor a cero.";
               break;
             case "numero_palmas_check":
-              text = "El número de palmas deben ser mayor a cero";
+              text = "El número de palmas deben ser mayor a cero.";
               break;
             case "año_siembra_check":
-              text = "El año siembra debe ser mayor a cero";
+              text = "El año siembra debe ser mayor a cero.";
               break;
             case "material_siembra_check":
-              text = "El material de siembra no puede estar vacío";
+              text = "El material de siembra no puede estar vacío.";
               break;
             default:
-              text = "Error inesperado de base de datos";
+              text = "Error inesperado de base de datos.";
           }
           res
             .status(400)
-            .send({ message: `${text}. No se pudo registrar el nuevo lote` });
+            .send({ message: `${text}. No se pudo registrar el nuevo lote.` });
         });
     }
   });
@@ -158,14 +170,14 @@ rutas
           res
             .status(400)
             .send({
-              message: "No se pudo obtener la información de este lote",
+              message: "No se pudo obtener la información de este lote.",
             });
         } else {
           res.status(200).send(rta.rows);
         }
       })
       .catch((err) => {
-        res.status(400).send({ message: "Algo inesperado ocurrió" });
+        res.status(400).send({ message: "Algo inesperado ocurrió." });
         console.log(err);
       });
   })
@@ -174,32 +186,47 @@ rutas
       .then((rta) => {
         res.status(200).send({
           respuesta: rta.rows,
-          message: "El lote se actualizó correctamente",
+          message: "El lote se actualizó correctamente.",
         });
       })
       .catch((err) => {
         console.log(err);
         switch (err.constraint) {
           case "hectareas_check":
-            text = "Las hectáreas deben ser mayor a cero";
+            text = "Las hectáreas deben ser mayor a cero.";
             break;
           case "numero_palmas_check":
-            text = "El número de palmas deben ser mayor a cero";
+            text = "El número de palmas deben ser mayor a cero.";
             break;
           case "año_siembra_check":
-            text = "El año siembra debe ser mayor a cero";
+            text = "El año siembra debe ser mayor a cero.";
             break;
           case "material_siembra_check":
-            text = "El material de siembra no puede estar vacío";
+            text = "El material de siembra no puede estar vacío.";
             break;
           default:
-            text = "Error inesperado de base de datos";
+            text = "Error inesperado de base de datos.";
         }
         res
           .status(400)
-          .send({ message: `${text}. No se pudo actualizar el lote` });
+          .send({ message: `${text}. No se pudo actualizar el lote.` });
       });
-  });
+  })
+  .delete(authorize(["admin"]), (req, res) => {
+          soft_delete_lote(decodeURIComponent(req.params["nombre"])).then(rta => {
+              //console.log("req.params", req.params);
+              if (!rta) {
+                  res.status(400).send({ message: 'No se pudo eliminar la enfermedad.' });
+              } else {
+                  res.status(200).send({ message: 'Se eliminó la enfermedad.' });
+              }
+          }).catch(err => {
+              console.log(err);
+              //console.log("req.params", req.params);
+              res.status(500).send({ message: 'Algo inesperado ocurrió.' })
+          })
+      })
+
 
 rutas
   .route("/lote/mapa/:nombre")
@@ -207,7 +234,7 @@ rutas
     get_lote_mapa(req)
       .then((rta) => {
         if (!rta) {
-          res.status(400).send({ message: "No se pudo obtener el mapa" });
+          res.status(400).send({ message: "No se pudo obtener el mapa." });
         } else {
           res.set("Content-Type", "application/vnd.google-earth.kml+xml");
           res.header("Content-Type", "application/vnd.google-earth.kml+xml");
@@ -215,7 +242,7 @@ rutas
         }
       })
       .catch((err) => {
-        res.status(400).send({ message: `Algo inesperado ocurrió` });
+        res.status(400).send({ message: `Algo inesperado ocurrió.` });
         console.log(err);
       });
   })
@@ -224,12 +251,12 @@ rutas
       .then((rta) => {
         res.status(200).send({
           respuesta: rta.rows,
-          message: "El lote se actualizó correctamente",
+          message: "El lote se actualizó correctamente.",
         });
       })
       .catch((err) => {
         console.log(err);
-        res.status(400).send("No se pudo actualizar el lote");
+        res.status(400).send("No se pudo actualizar el lote.");
       });
   });
 
